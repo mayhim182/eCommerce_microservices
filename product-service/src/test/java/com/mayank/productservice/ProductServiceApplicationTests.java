99982022ffd59c67024ba836094654d5c829a43b
mayhim182 <mayankatnitrkl@gmail.com>
2023-06-16 10:24:07 +0530

    adding ProductServiceApplicationTests file

--- product-service/src/test/java/com/mayank/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/mayank/productservice/ProductServiceApplicationTests.java
@@ -1,15 +1,25 @@
 package com.mayank.productservice;
 
+import com.mayank.productservice.dto.ProductRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,9 +34,29 @@ class ProductServiceApplicationTests {
   }
   @Autowired
   private MockMvc mockMvc;
+  @Autowired
+  private ObjectMapper objectMapper;
 
   @Test
-	void shouldCreateProduct() {
+	void shouldCreateProduct() throws Exception {
+
+    ProductRequest productRequest = getProductRequest();
+
+    String productRequestString = objectMapper.writeValueAsString(productRequest);
+
+    mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(productRequestString))
+      .andExpect(status().isCreated());
 	}
+
+  private ProductRequest getProductRequest(){
+    return ProductRequest.builder()
+      .name("Iphone 13")
+      .description("Iphone 13")
+      .price(BigDecimal.valueOf(1200))
+      .build();
+  }
+
 
 }
